237363565ebc3cbeb4e45d62b99a49dd9c98a872
Adam Hirst <adam@ahirst.com>
2018-04-14 15:52:43 +0100

    Public updates

--- public/js/transcri.me.js
+++ public/js/transcri.me.js
@@ -1,59 +1,56 @@
 // transcri.me.js - Text to Speech
+function startTranscribing(socket, cb) {
+    var output = document.getElementById("output");
+    var final_span = document.getElementById("final");
+    var interim_span = document.getElementById("temp");
+    var final_transcript = '';
 
-var output = document.getElementById("output");
-var final_span = document.getElementById("final");
-var interim_span = document.getElementById("temp");
-var final_transcript = '';
-
-if (!('webkitSpeechRecognition' in window)) {
-    upgrade();
-} else {
-    console.log("Start recognition");
-    var recognition = new webkitSpeechRecognition();
-    recognition.continuous = true;
-    recognition.interimResults = true;
-
+    if (!('webkitSpeechRecognition' in window)) {
+        upgrade();
+    } else {
+        console.log("Start recognition");
+        var recognition = new webkitSpeechRecognition();
+        recognition.continuous = true;
+        recognition.interimResults = true;
     
-    recognition.onstart = function() { 
         
-    console.log("Started recognition");
-    }
-
-    recognition.onresult = function(event) {
-        
-        var interim_transcript = '';
+        recognition.onstart = function() {
+            console.log("Started recognition");
+        }
     
-        for (var i = event.resultIndex; i < event.results.length; ++i) {
-            if (event.results[i].isFinal) {
-            final_transcript += event.results[i][0].transcript;
-            } else {
-            interim_transcript += event.results[i][0].transcript;
+        recognition.onresult = function(event) {
             
+            var interim_transcript = '';
+        
+            for (var i = event.resultIndex; i < event.results.length; ++i) {
+                if (event.results[i].isFinal) {
+                final_transcript += event.results[i][0].transcript;
+                } else {
+                interim_transcript += event.results[i][0].transcript;
+                
+                }
+            }
+            if (interim_transcript == "") {
+                cb(final_transcript);
+    
+                final_transcript = "";
+    
+                //socket.emit('final', final_transcript);
+                console.log("end of statement");
+            } else {
+                final_span.innerHTML = final_transcript;
+                interim_span.innerHTML = interim_transcript;
+                // socket.emit('interim', final_transcript + interim_transcript);
             }
         }
-        if (interim_transcript == "") {
-            var p = document.createElement("p");
-            var textnode = document.createTextNode(final_transcript);
-            p.appendChild(textnode);
-            output.insertBefore(p, final_span);  
-
-            final_transcript = "";
-
-            console.log("end of statement");
+    
+        recognition.onerror = function(event) { 
+            console.log(event);
         }
-
-        final_span.innerHTML = final_transcript;
-        interim_span.innerHTML = interim_transcript;
-
-        
+    
+        recognition.onend = function() { 
+            console.log("Ended");
+        }
+        recognition.start();
     }
-
-    recognition.onerror = function(event) { 
-        console.log(event);
-    }
-
-    recognition.onend = function() { 
-        console.log("Ended"); 
-    }
-    recognition.start();
 }
